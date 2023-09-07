8ac663269d49687fc6b60f4536d87ae863c4d7a3
miguelMedinaCastro <miguelmedinadecastro@gmail.com>
2023-09-07 02:21:44 -0300

    commit

---
+++ exercicios-python/sucessorEAntecessor.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+num = int(input("Informe um número: "))
+
+print("O sucessor é", num + 1)
+print("O antecessor é", num - 1)
