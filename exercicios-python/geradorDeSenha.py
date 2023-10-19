af205b560d24abfd13fc4403b6a2968b33a7dae3
Miguel Medina <135060808+miguelMedinaCastro@users.noreply.github.com>
2023-10-19 09:03:43 -0300

    commit

--- exercicios-python/geradorDeSenha.py
+++ exercicios-python/geradorDeSenha.py
@@ -1,12 +1,12 @@
 import random #módulo importante para decisão randômica disponível no próprio python
 
-lower = "abcdefghijklmnopqrstuvwxyz" #string com os alfabetos todos minúsculos 
-upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" #string com os alfabetos todos em maiúsculos
-special = "!@#$%*()[]{}\|,.;:?/°º=-_+'" #string com os caracteres especiais
-num = "0123456789" #string com números
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+special = "!@#$%*()[]{}\|,.;:?/°º=-_+'"
+num = "0123456789" 
 
-all = lower + upper + special + num #concatenação com as 4 variáveis
-length = 20 #tamanho da senha
-password = "".join(random.sample(all, length)) #password é uma string vazia, '.join()' irá acrescentar em password elementos aleatórios graças a função 'random.sample(var, var determinante)'
+all = lower + upper + special + num 
+length = 20 
+password = "".join(random.sample(all, length)) 
 
 print(password) #print password
