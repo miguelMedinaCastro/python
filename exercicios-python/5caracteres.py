23cf09a44a28359cd2ab02f46bbfe2857458bb1d
Miguel Medina <135060808+miguelMedinaCastro@users.noreply.github.com>
2023-10-19 09:05:16 -0300

    commit

--- exercicios-python/5caracteres.py
+++ exercicios-python/5caracteres.py
@@ -1,10 +1,9 @@
 # Escreva um programa que leia uma lista de palavras do usuário e retorne outra lista contendo apenas as palavras com mais de 5 caracteres.
 
-user = input("Digite uma frase: ").lower() #input do user
-listUser = user.split() #função '.split()' para a str(user) virar uma list
-newList = [] #vazia, reservada para armazenar as palavras com mais de 5 caracteres
-
-for word in listUser: #word vai percorrer a listUser
-    if len(word) > 5: #se o tamanho de word(palavra) for maior do que 5
-        newList.append(word) #então a palavra é adicionada a newList
-print(newList) #print newList
+user = input("Digite uma frase: ").lower() 
+listUser = user.split() 
+newList = [] 
+for word in listUser:
+    if len(word) > 5:
+        newList.append(word) 
+print(newList) 
