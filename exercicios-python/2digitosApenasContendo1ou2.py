266fdbcfb57a64db147c8fa727e3f6354a5e1845
Miguel Medina <135060808+miguelMedinaCastro@users.noreply.github.com>
2024-01-03 18:12:17 -0300

    commit

--- exercicios-python/2digitosApenasContendo1ou2.py
+++ exercicios-python/2digitosApenasContendo1ou2.py
@@ -1,3 +1,4 @@
+# mostrar apenas números contendo 2 dígitos, múltiplos por 3 que tenham apenas os dígitos 1 ou 2.
 for i in range(10, 100):
     if i % 3 == 0:
         if "1" in str(i) or "2" in str(i):
