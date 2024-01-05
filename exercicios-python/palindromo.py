cf23091e7af61c6c6f3ca44d2cbdcd991367cf64
Miguel Medina <135060808+miguelMedinaCastro@users.noreply.github.com>
2024-01-05 17:10:17 -0300

    commit

--- exercicios-python/palindromo.py
+++ exercicios-python/palindromo.py
@@ -1,6 +1,3 @@
-#  Escreva um programa em python que leia um texto e diga se é ou não um palíndromo (ou seja, se o inverso da string é igual a ela). Não será possível utilizar qualquer função no python que trabalhe com strings.
-# Exemplos: Ama, mirim, A grama é amarga.
-
 phrase = input("Informe algo: ").lower()
 
 if phrase == phrase[::-1]:
