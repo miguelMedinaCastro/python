da239a72329fac42338e59d8171fddc32124395c
Miguel Medina <135060808+miguelMedinaCastro@users.noreply.github.com>
2024-01-05 17:08:25 -0300

    commit

--- exercicios-python/codigoMorse.py
+++ exercicios-python/codigoMorse.py
@@ -4,22 +4,19 @@ newString = [] #lista vazia
 
 morseCode = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----"] #código morse de A até Z(incluindo os de números de 0 a 9)
 
-#i vai percorrer letra por letra na string:
 for i in string: 
-    if 65 <= ord(i) <= 90: #se o i for maior ou igual ao código ASCII de A e ASCII de Z 
-        position = ord(i) - 65 #pega código ASCII do i(letra em questão) e realiza uma subtração de 65(ASCII do A)
-        newString.append(morseCode[position]) #assim, se pega a posição do A na lista, do B, C, D, .... até Z
-    #se tiver espaço na string:
+    if 65 <= ord(i) <= 90: 
+        position = ord(i) - 65 
+        newString.append(morseCode[position])
     elif " " in i: 
-        newString.append(" ") #apenas se adiciona um espaço, simples assim
-    #se o i não está entre letras:
+        newString.append(" ") 
     else: 
-        if 48 <= ord(i) <= 57: #se o i for maior ou igual ao ASCII de 0 e ASCII de 9
-            position = (ord(i) - 48) + 25 #pega código ASCII do i(número em questão) e realiza uma subtração de 48(ASCII de 0) & depois fazer uma soma 25(para saber em qual posição está o número após o alfabeto)
+        if 48 <= ord(i) <= 57: 
+            position = (ord(i) - 48) + 25 #realiza uma subtração de 48(ASCII de 0) & depois fazer + 25(para saber em qual posição está o número após o alfabeto)
             newString.append(morseCode[position]) #assim, se pega a posição do 0, 1, 2, ... até 9
 
-removeOne = "".join(str(newString).strip("[]")) #para retirar os colchetes
-removeTwo = removeOne.replace(",", "") #para retirar as vírgulas
-removeThree = removeTwo.replace("'", "") #para retirar as aspas
+removeOne = "".join(str(newString).strip("[]")) 
+removeTwo = removeOne.replace(",", "") 
+removeThree = removeTwo.replace("'", "") 
 
-print("Saída esperada:", removeThree) #printa e voilá
+print("Saída esperada:", removeThree) 
