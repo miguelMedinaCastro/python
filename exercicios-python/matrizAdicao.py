caad43261bd2e72314c6f7c0b613c02810b4bfe2
Miguel Medina <135060808+miguelMedinaCastro@users.noreply.github.com>
2024-03-05 20:46:09 -0300

    commit

--- exercicios-python/matrizAdicao.py
+++ exercicios-python/matrizAdicao.py
@@ -2,21 +2,19 @@
 
 matrizA = []
 
-#criação das matrizes 2 x 2 feita pelo usuário:
 
 print("Matriz A:")
 
-for i in range(2): #limitar range em 2
+for i in range(2): 
     linhaA = []
-    for j in range(2): #limitar range em 2, assim, i e j formam uma matriz quadrada, ou seja, i = j
-        valorA = int(input(f"Informe valores numéricos para as posições ({i + 1}, {j + 1}): ")) #usuario informa por posições já pré estabelecidas os valores numéricos, obs: "+ 1" para não começar com 0
-        linhaA.append(valorA) #i(linha) adicionados na lista vazia
-    matrizA.append(linhaA) #j(coluna) adicionados na lista vazia
+    for j in range(2): 
+        valorA = int(input(f"Informe valores numéricos para as posições ({i + 1}, {j + 1}): "))
+        linhaA.append(valorA) 
+    matrizA.append(linhaA)
 
 print("Matriz B:")
 
 matrizB = []
-#exatamente o mesmo processo da matriz A.
 for k in range(2):
     linhaB = []
     for l in range(2):
@@ -31,9 +29,9 @@ matrizC = []
 for m in range(2):
     linhaC = []
     for n in range(2):
-        add = matrizA[m][n] + matrizB[m][n] #novas variáveis para representar i,j,k e l, ocorre a soma por posição
+        add = matrizA[m][n] + matrizB[m][n] 
         linhaC.append(add)    
     matrizC.append(linhaC)
-#representação de uma matriz: 
+
 for linha in matrizC:
     print(linha)
